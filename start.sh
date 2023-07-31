88ed0ec8d65e78041068319c8143207a83fa10a4
Li Hua <13922667014@163.com>
2023-07-31 13:19:23 +0800

    add zgc

--- start.sh
+++ start.sh
@@ -2,9 +2,10 @@ CURDIR=$(cd $(dirname $0); pwd )
 cd $CURDIR
 
 java \
+	-Xmx1600M -XX:+UseZGC \
 	-jar ./paper-1.20.1-100.jar \
 	--host 0.0.0.0 \
 	--port 25565 \
 	--nogui \
 	--serverId loveumua \
-	
+pause
